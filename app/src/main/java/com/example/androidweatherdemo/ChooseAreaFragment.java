85b31ba3df4ba3ccd240734b8b61ec38b8b34f83
xujinfeng <1352176165@qq.com>
2020-05-30 16:48:27 +0800

    '遍历省市县三级列表’

---
+++ app/src/main/java/com/example/androidweatherdemo/ChooseAreaFragment.java
@@ -0,0 +1,210 @@
+package com.example.androidweatherdemo;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.androidweatherdemo.db.City;
+import com.example.androidweatherdemo.db.County;
+import com.example.androidweatherdemo.db.Province;
+import com.example.androidweatherdemo.util.HttpUtil;
+import com.example.androidweatherdemo.util.Utility;
+
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+
+    private ProgressDialog progressDialog;
+    private TextView titleView;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<String>();
+
+    private List<Province> provincesList;
+    private List<City> cityList;
+    private List<County> countyList;
+
+    private Province selectProvince;
+    private City selectCity;
+    private int currentLevel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.choose_area, container, false);
+        titleView = view.findViewById(R.id.title_text);
+        backButton = view.findViewById(R.id.back_button);
+        listView = view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(currentLevel == LEVEL_PROVINCE){
+                    selectProvince = provincesList.get(i);
+                    queryCities();
+                }else if(currentLevel == LEVEL_CITY){
+                    selectCity = cityList.get(i);
+                    queryCounties();
+                }
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(currentLevel == LEVEL_COUNTY){
+                    queryCities();
+                }else if(currentLevel == LEVEL_CITY){
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    private void queryProvinces(){
+        titleView.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provincesList = DataSupport.findAll(Province.class);
+        if(provincesList.size()>0){
+            dataList.clear();
+            for(Province province : provincesList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+    }
+
+    private void queryCities(){
+        titleView.setText(selectProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList = DataSupport.where("provinceid = ?",String.valueOf(selectProvince.getId())).find(City.class);
+        if(cityList.size()>0){
+            dataList.clear();
+            for(City city : cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        }else{
+            int provinceCode = selectProvince.getProvinceCode();
+            String address = "http://guolin.tech/api/china/" + provinceCode;
+            queryFromServer(address,"city");
+        }
+    }
+
+    private void queryCounties(){
+        titleView.setText(selectCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countyList = DataSupport.where("cityid = ?",String.valueOf(selectCity.getId())).find(County.class);
+        if(countyList.size()>0){
+            dataList.clear();
+            for(County county : countyList){
+                dataList.add(county.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_COUNTY;
+        }else{
+            int provinceCode = selectProvince.getProvinceCode();
+            int cityCode = selectCity.getCityCode();
+            String address = "http://guolin.tech/api/china/" + provinceCode +"/"+ cityCode;
+            queryFromServer(address,"county");
+        }
+    }
+
+    private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                }else if("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText,selectProvince.getId());
+                }else if("county".equals(type)){
+                    result = Utility.handleCountyResponse(responseText,selectCity.getId());
+                }
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }else if("city".equals(type)){
+                                queryCities();
+                            }else if("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void showProgressDialog(){
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+
+    private void closeProgressDialog(){
+        if(progressDialog != null){
+            progressDialog.dismiss();
+        }
+    }
+}
--- app/src/main/java/com/example/androidweatherdemo/util/Utility.java
+++ app/src/main/java/com/example/androidweatherdemo/util/Utility.java
@@ -1,6 +1,8 @@
 package com.example.androidweatherdemo.util;
 
+import android.nfc.Tag;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.androidweatherdemo.db.City;
 import com.example.androidweatherdemo.db.County;
@@ -11,15 +13,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utility {
+
+    private static final String TAG = "Utility";
 /**
  * 解析和处理服务器返回的省级数据
  */
     public static boolean handleProvinceResponse(String response){
+        Log.d(TAG, "handleProvinceResponse: "+response);
         if(!TextUtils.isEmpty(response)){
             try {
-                JSONArray allProvince = new JSONArray(response);
-                for(int i =0;i<allProvince.length();i++){
-                    JSONObject provinceObject = allProvince.getJSONObject(i);
+                JSONArray allProvinces = new JSONArray(response);
+                for(int i =0;i<allProvinces.length();i++){
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
@@ -37,11 +42,12 @@ public class Utility {
      * 解析和处理服务器返回的城市级数据
      */
     public static boolean handleCityResponse(String response,int provinceId){
+        Log.d(TAG, "handleCityResponse: "+response);
         if(!TextUtils.isEmpty(response)){
             try {
-                JSONArray allCitys = new JSONArray(response);
-                for(int i = 0;i<allCitys.length();i++){
-                    JSONObject cityObject = allCitys.getJSONObject(i);
+                JSONArray allCities = new JSONArray(response);
+                for(int i = 0;i<allCities.length();i++){
+                    JSONObject cityObject = allCities.getJSONObject(i);
                     City city = new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
